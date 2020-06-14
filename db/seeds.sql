535ada236defb02c57a732123e3656847c480546
Romi Keerbs <rskeerbs@gmail.com>
2020-06-14 04:57:28 -0700

    First commit, need to fix errors

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers (name) VALUES ('Turkey burger');
+INSERT INTO burgers (name) VALUES ('Mushroom burger');
+INSERT INTO burgers (name, devoured) VALUES ('Barbeque bacon burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Avocado burger', true);
+INSERT INTO burgers (name) VALUES ('Jalapeno burger');
