8449d6ac9615d86c23caf4d7d50e829826d27674
Romi Keerbs <rskeerbs@gmail.com>
2020-06-22 07:56:05 -0700

    finishing touches added

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-devour").on("click", function(event) {
+  $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
     var newDevour = $(this).data("newdevour");
 
@@ -26,7 +26,7 @@ $(function() {
     event.preventDefault();
 
     var newBurger = {
-      name: $("#ca").val().trim(),
+      name: $("#burger").val().trim(),
       devoured: $("[name=devoured]:checked").val().trim()
     };
 
